152791cc0f05e0bfba5bc110afb1834ef692e8e1
gaeun <geun5744@gmail.com>
2023-07-04 13:12:59 +0900

    [ADD] LogoutActivity 생성

---
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/main/LogoutActivity.kt
@@ -0,0 +1,13 @@
+package com.puzzling.aoslaboratory.presentation.main
+
+import android.os.Bundle
+import com.puzzling.aoslaboratory.R
+import com.puzzling.aoslaboratory.base.BaseActivity
+import com.puzzling.aoslaboratory.databinding.ActivityLogoutBinding
+
+class LogoutActivity : BaseActivity<ActivityLogoutBinding>(R.layout.activity_logout) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
