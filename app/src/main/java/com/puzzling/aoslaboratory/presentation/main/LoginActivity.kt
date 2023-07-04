50f08112fe06fa50ed68b83ddd529f53215cf3af
gaeun <geun5744@gmail.com>
2023-07-04 13:12:47 +0900

    [ADD] LoginActivity 생성

---
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/main/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.puzzling.aoslaboratory.presentation.main
+
+import android.os.Bundle
+import com.puzzling.aoslaboratory.R
+import com.puzzling.aoslaboratory.base.BaseActivity
+import com.puzzling.aoslaboratory.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
