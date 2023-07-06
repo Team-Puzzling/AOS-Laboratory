66957e6e11e202b33c8afb72c646b0330beecf14
gaeun <geun5744@gmail.com>
2023-07-06 21:14:13 +0900

    [FEAT] acitivy에 카카오 로그인 기능 구현

--- app/src/main/java/com/puzzling/aoslaboratory/presentation/main/LoginActivity.kt
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/main/LoginActivity.kt
@@ -1,12 +1,50 @@
 package com.puzzling.aoslaboratory.presentation.main
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.puzzling.aoslaboratory.R
 import com.puzzling.aoslaboratory.base.BaseActivity
+import com.puzzling.aoslaboratory.data.ViewModelFactory
+import com.puzzling.aoslaboratory.data.service.KakaoLoginService
 import com.puzzling.aoslaboratory.databinding.ActivityLoginBinding
+import kotlinx.coroutines.launch
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    private val kakaoLoginService = KakaoLoginService(this)
+    private val viewModel: LoginViewModel by viewModels { ViewModelFactory(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startKakaoLogin()
+        isKakaoLoginSuccess()
+    }
+
+    private fun startKakaoLogin() {
+        binding.btnLoginKakao.setOnClickListener {
+            kakaoLoginService.startKakaoLogin(viewModel.kakaoLoginCallback)
+        }
+    }
+
+    private fun isKakaoLoginSuccess() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.isKakaoLogin.collect { isLoginSuccess ->
+                    if (isLoginSuccess) {
+                        val intent = Intent(this@LoginActivity, LogoutActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+
+            // `lifecycle` is DESTROYED when the coroutine resumes. repeatOnLifecycle
+            // suspends the execution of the coroutine until the lifecycle is DESTROYED.
+        }
     }
 }
--- app/src/main/java/com/puzzling/aoslaboratory/presentation/main/LogoutActivity.kt
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/main/LogoutActivity.kt
@@ -1,13 +1,52 @@
 package com.puzzling.aoslaboratory.presentation.main
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.puzzling.aoslaboratory.R
 import com.puzzling.aoslaboratory.base.BaseActivity
+import com.puzzling.aoslaboratory.data.ViewModelFactory
 import com.puzzling.aoslaboratory.databinding.ActivityLogoutBinding
+import kotlinx.coroutines.launch
 
 class LogoutActivity : BaseActivity<ActivityLogoutBinding>(R.layout.activity_logout) {
 
+    private val viewModel: LogoutViewModel by viewModels { ViewModelFactory(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        kakaoLogout()
+        kakaoDeleteAccount()
+        isLogoutSuccess()
+    }
+
+    private fun kakaoLogout() {
+        binding.btnLogoutLogoutKakao.setOnClickListener {
+            viewModel.kakaoLogout()
+        }
+    }
+
+    private fun kakaoDeleteAccount() {
+        binding.btnLogoutDeleteAccountKakao.setOnClickListener {
+            viewModel.kakaoDeleteAccount()
+        }
+    }
+
+    private fun isLogoutSuccess() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.isKakaoLogout.collect { isLoginSuccess ->
+                    if (isLoginSuccess) {
+                        val intent = Intent(this@LogoutActivity, LoginActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
