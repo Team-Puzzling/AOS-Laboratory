1d31b3a7c8bec27f016ee0b6f8ee72774313fb9a
gaeun <geun5744@gmail.com>
2023-07-06 19:06:19 +0900

    [FIX] companion object 제거

--- app/src/main/java/com/puzzling/aoslaboratory/util/PuzzlingApplication.kt
+++ app/src/main/java/com/puzzling/aoslaboratory/util/PuzzlingApplication.kt
@@ -3,17 +3,13 @@ package com.puzzling.aoslaboratory.util
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.puzzling.aoslaboratory.BuildConfig.NATIVE_APP_KEY
-import com.puzzling.aoslaboratory.data.SharedPreferences
+import com.puzzling.aoslaboratory.data.source.local.SharedPreferences
 
 class PuzzlingApplication : Application() {
-
-    companion object {
-        lateinit var sharedPreferences: SharedPreferences
-    }
 
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, NATIVE_APP_KEY)
-        sharedPreferences.init(this)
+        SharedPreferences.init(this)
     }
 }
